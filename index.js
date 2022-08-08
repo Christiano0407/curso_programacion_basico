4c75d3f216780e22e2478595d6cdfd7c8d51b644
Chris_Velázquez04 <placebo0704@gmail.com>
2022-08-08 21:02:59 +0000

    buttons and game part 01

--- index.js
+++ index.js
@@ -4,3 +4,34 @@ console.log("Hello World");
 name = prompt("What is your name?");
 console.log(`Hello Mrs. ${name}`); */
 //** === Algorithm === */
+/* const player = 0;
+player = prompt("Qué escojes: Piedra(2), Papel(3) o Tijera(4)")
+const pc = 3;
+
+if (player == 1) {
+  console.log("Piedra");
+} else {
+  console.log("Error!");
+} */
+// 1 es piedra, 2 es papel, 3 es tijera
+let jugador = 0;
+jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera");
+let pc = 2;
+
+const piedra = document.querySelector(`#piedra`);
+const papel = document.querySelector(`#papel`);
+const tijeraa = document.querySelector(`#tijera`);
+
+// alert("Elegiste " + jugador)
+function game() {
+  if (jugador == 1) {
+    console.log("Elegiste piedra");
+  } else if (jugador == 2) {
+    console.log("Elegiste papel");
+  } else if (jugador == 3) {
+    console.log("Elegiste tijera");
+  } else {
+    console.log("Elegiste PERDER");
+  }
+}
+game();
