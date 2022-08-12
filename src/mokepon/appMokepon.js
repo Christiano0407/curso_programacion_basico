47c83696356b93a43b2114788afc86f984e9eb65
Chris_Velázquez04 <placebo0704@gmail.com>
2022-08-12 04:13:25 +0000

    add game part 03

--- src/mokepon/appMokepon.js
+++ src/mokepon/appMokepon.js
@@ -1,5 +1,10 @@
 //** === Mokepon Variables ===  */
+const btnFire = document.querySelector(`#btnMokepon-fire`);
+console.log(btnFire);
+const btnWater = document.querySelector(`#btnMokepon-water`);
+const btnEarth = document.querySelector(`#btnMokepon-earth`);
 
+//*TODO ===  === */
 const btnMokepon = document.querySelector(`#btnMokepon`);
 btnMokepon.addEventListener("click", () => {
   console.log("Click Mokepon");
