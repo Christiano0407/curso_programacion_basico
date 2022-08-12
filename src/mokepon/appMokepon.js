f56220d65aaeaf70d61bfc2e41cc5e73299de791
Chris_Velázquez04 <placebo0704@gmail.com>
2022-08-12 06:56:28 +0000

    game part 06 and select animal

--- src/mokepon/appMokepon.js
+++ src/mokepon/appMokepon.js
@@ -1,14 +1,15 @@
 //** === Mokepon Variables ===  */
 const btnFire = document.querySelector(`#btnMokepon-fire`);
-console.log(btnFire);
 const btnWater = document.querySelector(`#btnMokepon-water`);
-console.log(btnWater);
 const btnEarth = document.querySelector(`#btnMokepon-earth`);
-console.log(btnEarth);
-const btnSelect = document.querySelector("#btnSelect");
+
+const btnSelectMascot = document.querySelector("#IdBtnSelect");
 const btnNewGame = document.querySelector(`#IdNewGame`);
 
 //*TODO ===  ===  Code === === */
+btnSelectMascot.addEventListener("click", () => {
+  console.log("Select Animal");
+});
 
 //** === Attack === */
 
