1f8086637fb2ff15d1d8fee8af672703875614d0
Chris_Velázquez04 <placebo0704@gmail.com>
2022-08-12 14:56:52 +0000

    true

--- src/mokepon/appMokepon.js
+++ src/mokepon/appMokepon.js
@@ -1,19 +1,39 @@
 //** === Mokepon Variables ===  */
+//*! === Buttons */
 const btnFire = document.querySelector(`#btnMokepon-fire`);
 const btnWater = document.querySelector(`#btnMokepon-water`);
 const btnEarth = document.querySelector(`#btnMokepon-earth`);
-
+//*! === Buttons Select and Init Game */
 const btnSelectMascot = document.querySelector("#IdBtnSelect");
 const btnNewGame = document.querySelector(`#IdNewGame`);
+//*! === Mokepon Monsters */
+const waterBomb = document.querySelector("#waterBomb");
+const punchEarth = document.querySelector("#punchEarth");
 
 //*TODO ===  ===  Code === === */
-btnSelectMascot.addEventListener("click", () => {
-  function selectMascot() {
-    return console.log("Select Animal");
-  }
-  selectMascot();
-});
+const initGame = () => {
+  btnSelectMascot.addEventListener("click", () => {
+    selectMascot();
+    checkedAdd();
+    checkedNot();
+  });
+};
 
-//** === Attack === */
+function checkedAdd() {
+  const monsterFire = (document.querySelector(`#monsterFire`).checked = true);
+  return console.log(`Select a MonsterFire: ${monsterFire}`);
+}
 
-//*! === Message === */
+function checkedNot() {
+  const monsterFire = (document.querySelector(`#monsterFire`).checked = false);
+  return monsterFire;
+}
+
+const selectMascot = () => {
+  return console.log("Select Animal");
+};
+
+//** === Init Game Window === */
+window.addEventListener("load", initGame);
+
+//** === Message === */
