5e0e869afd4cba04dccee57b55e3fd623b19cdd8
Chris_Velázquez04 <placebo0704@gmail.com>
2022-08-12 05:46:06 +0000

    game part 03 buttons

--- src/mokepon/appMokepon.js
+++ src/mokepon/appMokepon.js
@@ -2,9 +2,13 @@
 const btnFire = document.querySelector(`#btnMokepon-fire`);
 console.log(btnFire);
 const btnWater = document.querySelector(`#btnMokepon-water`);
+console.log(btnWater);
 const btnEarth = document.querySelector(`#btnMokepon-earth`);
+console.log(btnEarth);
+const btnSelect = document.querySelector("#btnSelect");
+const btnNewGame = document.querySelector(`#IdNewGame`);
 
-//*TODO ===  === */
+//*TODO ===  ===  Code === === */
 
 //** === Attack === */
 
