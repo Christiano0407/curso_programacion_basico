ec973251de5675ddcbf85369366d691e8c5f9bcc
Chris_Velázquez04 <placebo0704@gmail.com>
2022-08-14 18:19:21 +0000

    select add and remove

--- src/mokepon/appMokepon.js
+++ src/mokepon/appMokepon.js
@@ -22,22 +22,39 @@ const initGame = () => {
 const checkedAdd = () => {
   if (monsterFire.checked) {
     alert(`Your Select: MonsterFire 🔥`);
+    fireAdd();
   } else if (waterBomb.checked) {
     alert(`Your Select: WaterBomb 💧`);
+    waterAdd();
   } else if (punchEarth.checked) {
     alert(`Your Select: PunchEarth 🌍🦾`);
   } else {
-    alert("Sorry! Your Not Select One Mokepon");
+    alert("Sorry!! Your Not Select One. Please. select your Mokepon");
   }
 };
 
 //*! === Add Chield Elements */
-const img = document.createElement("img");
-img.src = "https://images.pexels.com/photos/1716861/pexels-photo-1716861.jpeg";
-img.width = 200;
-img.height = 150;
-img.className = "monsterImg";
-divMonster.appendChild(img);
+function fireAdd() {
+  const img = document.createElement("img");
+  img.src =
+    "https://images.unsplash.com/photo-1643725173053-ed68676f1878?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+  /*  "https://images.pexels.com/photos/1716861/pexels-photo-1716861.jpeg"; */
+  img.width = 200;
+  img.height = 150;
+  img.className = "monsterImg";
+  divMonster.appendChild(img);
+}
+
+function waterAdd() {
+  const img = document.createElement("img");
+  img.src =
+    "https://images.unsplash.com/photo-1596743343697-bd2c1e5a8c81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+  /*  "https://images.pexels.com/photos/1716861/pexels-photo-1716861.jpeg"; */
+  img.width = 200;
+  img.height = 150;
+  img.className = "monsterImg";
+  divMonster.appendChild(img);
+}
 //** === BATTLE */
 const selectBattleMascot = () => {};
 
