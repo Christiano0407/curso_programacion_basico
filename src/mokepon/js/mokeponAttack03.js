ddc95b68da07a9cd30f5129adbc51415e1cb9e86
Chris_Velázquez04 <placebo0704@gmail.com>
2022-08-15 14:49:23 +0000

    attacks part 03

--- src/mokepon/js/mokeponAttack03.js
+++ src/mokepon/js/mokeponAttack03.js
@@ -1,13 +1,27 @@
 //** === Attack */
+const divAttack = document.querySelector(`#IdChildAttack`);
 let playAttack;
 
-//*! === Function Attack Fire */
+//*? === Function Attack Fire */
 export const fireAttack = () => {
-  playAttack = console.log("Fire");
+  playAttack = imageAttackFire();
 };
 export const waterAttack = () => {
-  playAttack = console.log("Water");
+  playAttack = imageAttackWater();
 };
 export const earthAttack = () => {
-  playAttack = console.log("Rock Attack!!");
+  playAttack = imageAttackEarth();
 };
+
+//*! === ADD Images Child */
+function imageAttackFire() {
+  let imageFire = document.createElement("img");
+  imageFire.src =
+    "https://images.unsplash.com/photo-1583248352195-d3a8e766edf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80";
+  imageFire.width = 100;
+  imageFire.height = 100;
+  imageFire.className = "fireImage";
+  divAttack.appendChild(imageFire);
+}
+function imageAttackWater() {}
+function imageAttackEarth() {}
