bf15e217ad3fd624f622354adff5143123dabefd
Chris_Velázquez04 <placebo0704@gmail.com>
2022-08-15 15:01:09 +0000

    fire attacks

--- src/mokepon/js/mokeponAttack03.js
+++ src/mokepon/js/mokeponAttack03.js
@@ -2,7 +2,7 @@
 const divAttack = document.querySelector(`#IdChildAttack`);
 let playAttack;
 
-//*? === Function Attack Fire */
+//*? === Function Attack Fire, Water and Earth === */
 export const fireAttack = () => {
   playAttack = imageAttackFire();
 };
@@ -23,5 +23,21 @@ function imageAttackFire() {
   imageFire.className = "fireImage";
   divAttack.appendChild(imageFire);
 }
-function imageAttackWater() {}
-function imageAttackEarth() {}
+function imageAttackWater() {
+  let imageWater = document.createElement("img");
+  imageWater.src =
+    "https://images.unsplash.com/photo-1527066236128-2ff79f7b9705?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80";
+  imageWater.width = 100;
+  imageWater.height = 100;
+  imageWater.className = "waterImage";
+  divAttack.appendChild(imageWater);
+}
+function imageAttackEarth() {
+  let imageRock = document.createElement("img");
+  imageRock.src =
+    "https://images.unsplash.com/photo-1584715642381-6f1c4b452b1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+  imageRock.width = 100;
+  imageRock.height = 100;
+  imageRock.className = "rockImage";
+  divAttack.appendChild(imageRock);
+}
